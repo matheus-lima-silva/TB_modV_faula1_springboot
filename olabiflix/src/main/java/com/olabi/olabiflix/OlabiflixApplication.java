8de230282d1efade35c4fdd3b47590fd98d221d9
matheus-lima-silva <150077612+matheus-lima-silva@users.noreply.github.com>
2024-05-26 16:49:16 -0300

    Feat:  filme somente conforme aula

--- olabiflix/src/main/java/com/olabi/olabiflix/OlabiflixApplication.java
+++ olabiflix/src/main/java/com/olabi/olabiflix/OlabiflixApplication.java
@@ -1,20 +1,38 @@
 package com.olabi.olabiflix;
 
+import com.olabi.olabiflix.model.entity.Filme;
+import com.olabi.olabiflix.repository.FilmeRepository;
+import com.olabi.olabiflix.repository.SerieRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
 @RestController
 public class OlabiflixApplication {
 
+	private final FilmeRepository repository;
+
+
+
+	public OlabiflixApplication(FilmeRepository repository){
+		this.repository = repository;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(OlabiflixApplication.class, args);
 	}
 
 	@GetMapping("/hello")
 	public String hello(){
-		return "Salve, mundao!";
+		return "Salve, mundão!";
 	}
+
+	@GetMapping("/filmes")
+	public List<Filme> getFilmes(){
+		return repository.findAll();
+	}
+
 }
