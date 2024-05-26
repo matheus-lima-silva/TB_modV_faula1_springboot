dcd839995f06db166bccf154c17dcad0e59ed32a
matheus-lima-silva <150077612+matheus-lima-silva@users.noreply.github.com>
2024-05-26 16:45:12 -0300

    Feat: criacao classe filme conforme aula

---
+++ olabiflix/src/main/java/com/olabi/olabiflix/model/entity/Filme.java
@@ -0,0 +1,203 @@
+package com.olabi.olabiflix.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
+
+// @Entity Para criar tabela
+@Entity
+// @Table para definir nome da tabela *não obrigatório
+@Table(name = "filme")
+public class Filme {
+
+    // Campo ID chave primária
+    @Id
+    @UuidGenerator // Annotation específica para este caso de UUID
+    private UUID id;
+
+    // Poderiamos usar também a annotation @Column para dar nome ao campo/coluna da tabela
+    // *Não obrigatório
+    // @Column(name = "titulo")
+    private String title;
+
+    private String releaseYear;
+
+    private String rated;
+
+    private String released;
+
+    private String runtime;
+
+    private String genre;
+
+    private String director;
+
+    private String writer;
+
+    private String actors;
+
+    private String plot;
+
+    private String language;
+
+    private String country;
+
+    private String awards;
+
+    // Hibernate requer um construtor sem argumentos
+    // para instanciar a entidade durante operações de persistência no banco
+    protected Filme() {}
+
+    // Construtor sem o campo 'id' pois será gerado automaticamente
+
+    public Filme(String title, String releaseYear, String rated, String released, String runtime, String genre, String director, String writer, String actors, String plot, String language, String country, String awards) {
+        this.title = title;
+        this.releaseYear = releaseYear;
+        this.rated = rated;
+        this.released = released;
+        this.runtime = runtime;
+        this.genre = genre;
+        this.director = director;
+        this.writer = writer;
+        this.actors = actors;
+        this.plot = plot;
+        this.language = language;
+        this.country = country;
+        this.awards = awards;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(String releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public String getRated() {
+        return rated;
+    }
+
+    public void setRated(String rated) {
+        this.rated = rated;
+    }
+
+    public String getReleased() {
+        return released;
+    }
+
+    public void setReleased(String released) {
+        this.released = released;
+    }
+
+    public String getRuntime() {
+        return runtime;
+    }
+
+    public void setRuntime(String runtime) {
+        this.runtime = runtime;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public String getActors() {
+        return actors;
+    }
+
+    public void setActors(String actors) {
+        this.actors = actors;
+    }
+
+    public String getPlot() {
+        return plot;
+    }
+
+    public void setPlot(String plot) {
+        this.plot = plot;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getAwards() {
+        return awards;
+    }
+
+    public void setAwards(String awards) {
+        this.awards = awards;
+    }
+
+    @Override
+    public String toString() {
+        return "Filme{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", releaseYear='" + releaseYear + '\'' +
+                ", rated='" + rated + '\'' +
+                ", released='" + released + '\'' +
+                ", runtime='" + runtime + '\'' +
+                ", genre='" + genre + '\'' +
+                ", director='" + director + '\'' +
+                ", writer='" + writer + '\'' +
+                ", actors='" + actors + '\'' +
+                ", plot='" + plot + '\'' +
+                ", language='" + language + '\'' +
+                ", country='" + country + '\'' +
+                ", awards='" + awards + '\'' +
+                '}';
+    }
+}
